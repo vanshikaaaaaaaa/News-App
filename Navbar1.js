6f616d84d5d6ef32ce57e3aeecd2edafb2082305
Vanshika Rathod <85212298+vanshikaaaaaaaa@users.noreply.github.com>
2024-06-19 23:12:25 +0530

    Create Navbar1.js

---
+++ Navbar1.js
@@ -0,0 +1,18 @@
+// Components/NavBar1.js
+import React from "react";
+
+function NavBar1() {
+	return (
+		<div>
+			<nav className="navbar navbar-dark bg-dark">
+				<div className="container-fluid">
+					<a className="navbar-brand " href="/">
+						News Geek
+					</a>
+				</div>
+			</nav>
+		</div>
+	);
+}
+
+export default NavBar1;
